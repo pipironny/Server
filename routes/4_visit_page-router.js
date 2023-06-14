7f48858f44a04b708a7a41346059594f1e9fd2b7
Denis Loginov <lamm2ez@yandex.ru>
2023-06-14 14:48:47 +0700

    роут visit

--- routes/4_visit_page-router.js
+++ routes/4_visit_page-router.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
+// Промежуточное ПО (Middleware)
+const requireAuth = require('../middleware/user_auth-middleware');
+const requireAuthAndRole = require('../middleware/admin_auth-middleware');
+
 connection = require('../db/mysql-connection').connection;
 
 // 4 Страница отметки посещаемость конкретной пары, по id занятия:
-router.get('/api/visit_page/:id/', requireAuthAndRole, (req, res)=>{
+router.get('/api/visit_page/:id', (req, res)=>{
     const id = req.params.id; 
     
-    connection.query('SELECT students.id AS student_id, students.first_name, students.last_name, students.patronymic, attendance.visit FROM `schedule` JOIN attendance ON `schedule`.`id`= attendance.schedule_id JOIN subjects ON `schedule`.`subject_id` = subjects.id JOIN students ON attendance.student_id = students.id WHERE `schedule`.`id` = 1', id,
+    connection.query('SELECT students.id AS student_id, students.first_name, students.last_name, students.patronymic, attendance.visit FROM `schedule` JOIN attendance ON `schedule`.`id`= attendance.schedule_id JOIN subjects ON `schedule`.`subject_id` = subjects.id JOIN students ON attendance.student_id = students.id WHERE `schedule`.`id` = ?', id,
     (err, result) =>{
         if (err){
             console.error("Ошибка подключения " + err.message);
@@ -21,22 +25,31 @@ router.get('/api/visit_page/:id/', requireAuthAndRole, (req, res)=>{
     });
 });
 
-router.put('/api/visit_page/:id/', requireAuthAndRole, (req, res)=>{
-    const attendanceData = req.body;
-    // Создаем массив значений для каждой отметки
-    const attendanceArray = attendanceData.map((item) => [item.student_id, item.visit]); 
+router.put('/api/visit_page/PUT', (req, res)=>{
+    const scheduleId = req.body.scheduleId; // Идентификатор занятия
+    const attendance = req.body.attendance; // Массив посещаемости студентов
     
-    connection.query('INSERT INTO `attendance` (student_id, visit) VALUES ?', [attendanceArray],
-    (err, result) =>{
-        if (err){
-            console.error("Ошибка добавления " + err.message);
-            res.status(500).json({ error: 'Ошибка при добавлении' });
-            return;
-        }
-        else{
-            res.status(200).json({ message: 'Посещение успешно обновлено' });
-        }
+    attendance.forEach((student) =>{
+        const studentId = student.studentId;
+        const visit = student.visit;
+
+
+        connection.query('INSERT INTO `attendance` (schedule_id, student_id, visit) VALUES (?, ?, ?)', 
+        [scheduleId, studentId, visit],
+        (err, result) =>{
+            if (err){
+                console.error("Ошибка добавления " + err.message);
+                res.status(500).json({ error: 'Ошибка при добавлении' });
+                return;
+            }
+            else{
+                res.status(200).json({ message: 'Посещение успешно обновлено' });
+            }
+        });
+
+
     });
+
 });
 
 module.exports = router;
