3f16d2d5a3bb7652cacd7966bab73c37e1091133
Denis Loginov <lamm2ez@yandex.ru>
2023-06-15 18:42:45 +0700

    Изменил на использование jwt.verify

--- middleware/admin_auth-middleware.js
+++ middleware/admin_auth-middleware.js
@@ -17,33 +17,19 @@ requireAuthAndRole = function requireAuthAndRole(req, res, next) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
 
-  // Парсинг и декодирование токена
-  const tokenParts = authToken.split('.');
-  if (tokenParts.length !== 3) {
+  try {
+    const decodedToken = jwt.verify(authToken, process.env.SECRET_KEY);
+    const { Role } = decodedToken;
+
+    if (Role !== 2) {
+      return res.status(403).json({ message: 'Access denied' });
+    }
+
+    req.user = decodedToken;
+    next();
+  } catch (err) {
+    console.error(err);
     return res.status(401).json({ message: 'Unauthorized' });
   }
-
-/* после получения и разделения токена на три части, мы извлекаем полезную нагрузку (payload) из второй части токена. 
-Затем мы декодируем payload из формата base64 и преобразуем его в объект JavaScript с помощью JSON.parse.
-Далее, мы проверяем наличие декодированного токена и userId, чтобы убедиться, что токен корректный и содержит необходимую информацию. */
-  const payloadBase64 = tokenParts[1];
-  const payload = Buffer.from(payloadBase64, 'base64').toString('utf-8');
-  const decodedToken = JSON.parse(payload);
-
-  if (!decodedToken || !decodedToken.userId) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
-
-  req.user = decodedToken;
-
-  //console.log (decodedToken);
-
-  // Проверка роли пользователя в токене
-  if (decodedToken.Role !== 2) {
-    return res.status(403).json({ message: 'Forbidden' });
-  }
-
-  next();
-}
-
+};
 module.exports = requireAuthAndRole;
