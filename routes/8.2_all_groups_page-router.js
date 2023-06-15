a25ecb9d9264d4fdb416c6dc21ad3810d9da11d9
Denis Loginov <lamm2ez@yandex.ru>
2023-06-16 00:39:58 +0700

    Updated

--- routes/8.2_all_groups_page-router.js
+++ routes/8.2_all_groups_page-router.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+const jwt = require('jsonwebtoken');
+
 const requireAuth = require('../middleware/user_auth-middleware');
 
 connection = require('../db/mysql-connection').connection;
 
 // все группы по id преподавателя (id группы, название, количество дисциплин)
-router.get('/api/all_groups/:id', requireAuth,(req, res)=>{
-    const id = req.params.id; // id преподавателя
+router.get('/api/all_groups/', requireAuth,(req, res)=>{
+    const authHeader = req.headers.authorization;
+    const [authType, authToken] = authHeader.split(' ');
+    const decodedToken = jwt.verify(authToken, process.env.SECRET_KEY);
+    const id  = decodedToken.userId; // id преподавателя
     
     connection.query('SELECT groups.id, groups.name AS group_name, COUNT(DISTINCT schedule.subject_id) AS subject_count FROM `groups` JOIN `schedule` ON groups.id = `schedule`.`group_id` WHERE `schedule`.`employee_id` = ? GROUP BY groups.id, groups.name', id,
     (err, result) =>{
--- routes/8.3_all_subjects_page-router.js
+++ routes/8.3_all_subjects_page-router.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+const jwt = require('jsonwebtoken');
+
 const requireAuth = require('../middleware/user_auth-middleware');
 
 connection = require('../db/mysql-connection').connection;
 
 // все дисциплины преподавателя по id( id дисциплины, название, количество групп)
-router.get('/api/all_subjects/:id', requireAuth,(req, res)=>{
-    const id = req.params.id; // id преподавателя
+router.get('/api/all_subjects/', requireAuth,(req, res)=>{
+    const authHeader = req.headers.authorization;
+    const [authType, authToken] = authHeader.split(' ');
+    const decodedToken = jwt.verify(authToken, process.env.SECRET_KEY);
+    const id  = decodedToken.userId; // id преподавателя
     
     connection.query('SELECT subjects.id, subjects.name AS subject_name, COUNT(DISTINCT schedule.group_id) AS group_count FROM `subjects` JOIN `schedule` ON subjects.id = `schedule`.`subject_id` WHERE `schedule`.`employee_id` = ? GROUP BY subjects.id, subjects.name', id,
     (err, result) =>{
