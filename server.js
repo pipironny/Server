6ed3e3acde8fe564fd8ea3557802ac0758a81043
Denis Loginov <lamm2ez@yandex.ru>
2023-06-15 15:15:51 +0700

    Update

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(express.json());
 // Middleware для настройки CORS
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', `http://localhost:${PORT}`); // Здесь указывается домен вашей веб-страницы
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
