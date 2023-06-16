8d6a7413591f3f230db1424f3e1f4ae10eb8d023
Denis Loginov <lamm2ez@yandex.ru>
2023-06-16 13:50:43 +0700

    Добавлен 8.3 для admin

--- routes/8.3_all_subjects_admin.js
+++ routes/8.3_all_subjects_admin.js
@@ -9,9 +9,9 @@ const requireAuthAndRole = require('../middleware/admin_auth-middleware');
 connection = require('../db/mysql-connection').connection;
 
 // все дисциплины ( id дисциплины, название, количество групп)
-router.get('/api/all_subjects_admin/', requireAuth,(req, res)=>{
+router.get('/api/all_subjects_admin/', requireAuthAndRole,(req, res)=>{
 
-    connection.query('',
+    connection.query('SELECT subjects.id, subjects.name AS subject_name, COUNT(DISTINCT schedule.group_id) AS group_count FROM `subjects` JOIN `schedule` ON subjects.id = `schedule`.`subject_id` GROUP BY subjects.id, subjects.name',
     (err, result) =>{
         if (err){
             console.error("Ошибка подключения " + err.message);
