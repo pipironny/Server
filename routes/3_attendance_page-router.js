52c6c7e08e84cb246594f31535771e5b5a532386
Denis Loginov <lamm2ez@yandex.ru>
2023-06-16 18:17:42 +0700

    Добавлен attendance в title

--- routes/3_attendance_page-router.js
+++ routes/3_attendance_page-router.js
@@ -27,7 +27,7 @@ router.get('/api/attendance_page/:group_id/:subject_id', requireAuth,(req, res)=
     };
 
     // название дисциплины и группы
-    connection.query('SELECT subjects.name AS subject_name, groups.name AS group_name FROM subjects JOIN groups ON subjects.id = ? AND groups.id = ?',[subject_id, group_id],
+    connection.query('SELECT subjects.name AS subject_name, groups.name AS group_name, (SUM(CASE WHEN attendance.visit IN (1, 2) THEN 1 ELSE 0 END) / COUNT(*)) * 100 AS attendance_percentage FROM subjects JOIN `schedule` ON subjects.id = `schedule`.subject_id JOIN groups ON `schedule`.group_id = groups.id JOIN attendance ON `schedule`.id = attendance.schedule_id WHERE `schedule`.group_id = ? AND schedule.subject_id = ? GROUP BY subjects.name, groups.name',[group_id, subject_id],
     (err, result) =>{
         if (err){
             console.error("Ошибка подключения " + err.message);
