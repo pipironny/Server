b0b703408ac10f02048336518c7ccda2b8c548ed
Denis Loginov <lamm2ez@yandex.ru>
2023-06-16 15:13:43 +0700

    Update отправка date через params

--- routes/8.1_schedule_page-router.js
+++ routes/8.1_schedule_page-router.js
@@ -9,12 +9,12 @@ const requireAuth = require('../middleware/user_auth-middleware');
 connection = require('../db/mysql-connection').connection;
 
 // Расписание по дате и id преподавателя (id занятия, пара, дисциплина, группа, аудитория)
-router.get('/api/schedule_page/', requireAuth,(req, res)=>{
+router.get('/api/schedule_page/:date', requireAuth,(req, res)=>{
     const authHeader = req.headers.authorization;
     const [authType, authToken] = authHeader.split(' ');
     const decodedToken = jwt.verify(authToken, process.env.SECRET_KEY);
     const id  = decodedToken.userId; 
-    const date = req.body.date;
+    const date = req.params.date;
     
     connection.query('SELECT `schedule`.`id` AS schedule_id, `schedule`.`number` AS number, subjects.name AS subject_name, groups.name AS group_name, classrooms.number AS classroom FROM `schedule` JOIN subjects ON `schedule`.`subject_id` = subjects.id JOIN groups ON `schedule`.`group_id` = groups.id JOIN classrooms ON `schedule`.`classroom_id`= classrooms.id JOIN employees ON `schedule`.`employee_id` = employees.id WHERE employees.id = ? AND `schedule`.`date` = ? GROUP BY `schedule`.`number`', [id, date],
     (err, result) =>{
