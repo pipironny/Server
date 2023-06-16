5f103d5502685fbcf2a48528c54a93fb8d11bb46
Denis Loginov <lamm2ez@yandex.ru>
2023-06-16 22:28:53 +0700

    Update

--- backup/old_routes/4_visit_page-router.js
+++ backup/old_routes/4_visit_page-router.js
@@ -40,22 +40,51 @@ router.get('/api/visit_page/:id', requireAuth, (req, res)=>{
         }
     });
 
-    // id, ФИО и посещаемость(visit) всех студентов группы по этой дисциплине в отдельности
-    connection.query('SELECT students.id AS student_id, CONCAT(students.last_name, " ", students.first_name, " ", students.patronymic) AS full_name, attendance.visit FROM `schedule` JOIN attendance ON `schedule`.`id`= attendance.schedule_id JOIN subjects ON `schedule`.`subject_id` = subjects.id JOIN students ON attendance.student_id = students.id WHERE `schedule`.`id` = ?', id,
-    (err, result) =>{
-        if (err){
+    // Проверка принадлежности занятия преподавателю
+    connection.query('SELECT employee_id FROM schedule WHERE id = ? AND employee_id = ?', [id, teacher_id],
+    (err, result) => {
+        if (err) {
             console.error("Ошибка подключения " + err.message);
             res.status(500).json({ message: 'Internal Server Error' });
             return;
-        }
-        else{
-            body.visit = result;
-            if (
-                body.title &&
-                body.visit
-             ) {
-                res.json(body);
-             }
+        } else {
+            if (result.length === 0) {
+                res.status(403).json({ message: 'Access Denied' });
+                return;
+            }
+
+            // Дата пары, название дисциплины, название группы
+            connection.query('SELECT `schedule`.date, subjects.id AS subject_id, subjects.name AS subject_name, groups.name AS group_name FROM `schedule` JOIN subjects ON `schedule`.subject_id = subjects.id JOIN groups ON `schedule`.group_id = groups.id WHERE `schedule`.id = ?', id,
+            (err, result) =>{
+                if (err){
+                    console.error("Ошибка подключения " + err.message);
+                    res.status(500).json({ message: 'Internal Server Error' });
+                    return;
+                }
+                else{
+                    body.title = result;
+                }
+            });
+
+            // id, ФИО и посещаемость(visit) всех студентов группы по этой дисциплине в отдельности
+            connection.query('SELECT students.id AS student_id, CONCAT(students.last_name, " ", students.first_name, " ", students.patronymic) AS full_name, attendance.visit FROM `schedule` JOIN attendance ON `schedule`.`id`= attendance.schedule_id JOIN subjects ON `schedule`.`subject_id` = subjects.id JOIN students ON attendance.student_id = students.id WHERE `schedule`.`id` = ?', id,
+            (err, result) =>{
+                if (err){
+                    console.error("Ошибка подключения " + err.message);
+                    res.status(500).json({ message: 'Internal Server Error' });
+                    return;
+                }
+                else{
+                    body.visit = result;
+                    if (
+                        body.title &&
+                        body.visit
+                    ) {
+                        res.json(body);
+                    }
+                }
+            });
+
         }
     });
 
